632b2a9b4a1a7df97a261686b4b24354fbaaed6e
Hunter <hunter@lx-labs.com>
2015-08-03 06:03:36 -0400

    13:4 - Stock Search Implemented

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,46 +1,12 @@
 angular.module('yourAppsName.controllers', [])
 
 
-.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
+.controller('AppCtrl', ['$scope', 'modalService',
+  function($scope, modalService) {
 
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
+    $scope.modalService = modalService;
 
-  // Form data for the login modal
-  $scope.loginData = {};
-
-  // Create the login modal that we will use later
-  $ionicModal.fromTemplateUrl('templates/login.html', {
-    scope: $scope
-  }).then(function(modal) {
-    $scope.modal = modal;
-  });
-
-  // Triggered in the login modal to close it
-  $scope.closeLogin = function() {
-    $scope.modal.hide();
-  };
-
-  // Open the login modal
-  $scope.login = function() {
-    $scope.modal.show();
-  };
-
-  // Perform the login action when the user submits the login form
-  $scope.doLogin = function() {
-    console.log('Doing login', $scope.loginData);
-
-    // Simulate a login delay. Remove this and replace with your login
-    // code if using a login system
-    $timeout(function() {
-      $scope.closeLogin();
-    }, 1000);
-  };
-})
+}])
 
 
 
@@ -277,6 +243,33 @@ angular.module('yourAppsName.controllers', [])
       transitionDuration: 500
   	};
 
+}])
+
+
+
+.controller('SearchCtrl', ['$scope', '$state', 'modalService', 'searchService',
+  function($scope, $state, modalService, searchService) {
+
+    $scope.closeModal = function() {
+      modalService.closeModal();
+    };
+
+    $scope.search = function() {
+      $scope.searchResults = '';
+      startSearch($scope.searchQuery);
+    };
+
+    var startSearch = ionic.debounce(function(query) {
+      searchService.search(query)
+        .then(function(data) {
+          $scope.searchResults = data;
+        });
+    }, 400);
+
+    $scope.goToStock = function(ticker) {
+      modalService.closeModal();
+      $state.go('app.stock', {stockTicker: ticker});
+    };
 }])
 
 ;
--- www/js/services.js
+++ www/js/services.js
@@ -1,6 +1,50 @@
 angular.module('yourAppsName.services', [])
 
 
+.service('modalService', function($ionicModal) {
+
+  this.openModal = function(id) {
+
+    var _this = this;
+
+    if(id == 1) {
+      $ionicModal.fromTemplateUrl('templates/search.html', {
+        scope: null,
+        controller: 'SearchCtrl'
+      }).then(function(modal) {
+        _this.modal = modal;
+        _this.modal.show();
+      });
+    }
+    else if(id == 2) {
+      $ionicModal.fromTemplateUrl('templates/login.html', {
+        scope: $scope
+      }).then(function(modal) {
+        $scope.modal = modal;
+      });
+    }
+    else if(id == 3) {
+      $ionicModal.fromTemplateUrl('templates/login.html', {
+        scope: $scope
+      }).then(function(modal) {
+        $scope.modal = modal;
+      });
+    }
+  };
+
+  this.closeModal = function() {
+
+    var _this = this;
+
+    if(!_this.modal) return;
+    _this.modal.hide();
+    _this.modal.remove();
+  };
+
+})
+
+
+
 .factory('encodeURIService', function() {
   return {
     encode: function(string) {
@@ -378,6 +422,37 @@ angular.module('yourAppsName.services', [])
           deferred.reject();
           console.log("News error: " + error);
         });
+
+      return deferred.promise;
+    }
+  };
+})
+
+
+
+.factory('searchService', function($q, $http) {
+
+  return {
+
+    search: function(query) {
+
+      var deferred = $q.defer(),
+
+      url = 'http://d.yimg.com/autoc.finance.yahoo.com/autoc?query="' + query + '"&callback=YAHOO.Finance.SymbolSuggest.ssCallback';
+
+      YAHOO = window.YAHOO = {
+        Finance: {
+          SymbolSuggest: {}
+        }
+      };
+
+      YAHOO.Finance.SymbolSuggest.ssCallback = function(data) {
+        var jsonData = data.ResultSet.Result;
+        deferred.resolve(jsonData);
+      };
+
+      $http.jsonp(url)
+        .then(YAHOO.Finance.SymbolSuggest.ssCallback);
 
       return deferred.promise;
     }
